b305b7ff9252d3b8b537a0c13f6674fafba6ed72
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-11 17:57:43 +0300

    Completed lab

--- lib/seed.py
+++ lib/seed.py
@@ -1,55 +1,41 @@
-#!/usr/bin/env python3
+# models.py
 
-from faker import Faker
-import random
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
 
-from models import Game, Review
+Base = declarative_base(metadata=metadata)
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+class Game(Base):
+    __tablename__ = 'games'
 
-    session.query(Game).delete()
-    session.query(Review).delete()
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
 
-    fake = Faker()
+   
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
 
-    genres = ['action', 'adventure', 'strategy',
-        'puzzle', 'first-person shooter', 'racing']
-    platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
-        'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
-        'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
+class Review(Base):
+    __tablename__ = 'reviews'
 
-    games = []
-    for i in range(50):
-        game = Game(
-            title=fake.unique.name(),
-            genre=random.choice(genres),
-            platform=random.choice(platforms),
-            price=random.randint(5, 60)
-        )
-
-        # add and commit individually to get IDs back
-        session.add(game)
-        session.commit()
-
-        games.append(game)
-
-    reviews = []
-    for game in games:
-        for i in range(random.randint(1,5)):
-            review = Review(
-                score=random.randint(0, 10),
-                comment=fake.sentence(),
-                game_id=game.id
-            )
-
-            reviews.append(review)
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
     
-    session.bulk_save_objects(reviews)
-    session.commit()
-    session.close()
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
